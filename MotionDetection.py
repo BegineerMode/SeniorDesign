7b48310df234d534cce8ca700ff1538a313422c9
JazzWazzy <94722478+BegineerMode@users.noreply.github.com>
2024-10-28 16:24:26 -0500

    Update MotionDetection.py

--- MotionDetection.py
+++ MotionDetection.py
@@ -7,9 +7,9 @@ import cv2
 Debug = False
 
 # Create a VideoCapture object and read from input file
-cap = cv2.VideoCapture('C:/Users/reese/Downloads/coffee.mp4')
+cap = cv2.VideoCapture('C:/Users/reese/Downloads/coffee.mp4', )
 
-objdetect = cv2.createBackgroundSubtractorMOG2()
+objdetect = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=16)
 
 
 # Check if camera opened successfully
@@ -24,10 +24,18 @@ while(cap.isOpened()):
     if ret == True:
 
         mask = objdetect.apply(frame)
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            if area>20000:
+
+                #cv2.drawContours(frame, [cnt], -1, (0,255,0), 2)
+                x, y, w, h = cv2.boundingRect(cnt)
+                cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0))
 
     # Display the resulting frame
         cv2.imshow('Frame', frame)
-        cv2.imshow('mask', mask)
+        
         
     # Press Q on keyboard to exit
         if cv2.waitKey(25) & 0xFF == ord('q'):
