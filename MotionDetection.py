291af61a75fd10c50737de2774920fd0be25aced
JazzWazzy <94722478+BegineerMode@users.noreply.github.com>
2024-10-28 15:16:19 -0500

    Update MotionDetection.py
    objectdetection shit

--- MotionDetection.py
+++ MotionDetection.py
@@ -1,7 +1,45 @@
 # Code for Motion Detection
 
+import cv2
 
 
 
 Debug = False
 
+# Create a VideoCapture object and read from input file
+cap = cv2.VideoCapture('C:/Users/reese/Downloads/coffee.mp4')
+
+objdetect = cv2.createBackgroundSubtractorMOG2()
+
+
+# Check if camera opened successfully
+if (cap.isOpened()== False):
+    print("Error opening video file")
+
+# Read until video is completed
+while(cap.isOpened()):
+    
+# Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret == True:
+
+        mask = objdetect.apply(frame)
+
+    # Display the resulting frame
+        cv2.imshow('Frame', frame)
+        cv2.imshow('mask', mask)
+        
+    # Press Q on keyboard to exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+# Break the loop
+    else:
+        break
+
+# When everything done, release
+# the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
